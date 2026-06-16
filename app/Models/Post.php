8c58d34122cef24164510d8a9367411526a151a3
Manuel Alejandro <mplaza005@gmail.com>
2026-06-16 15:07:27 -0500

    holaaa

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Builder;
 
 class Post extends Model
 {
@@ -15,7 +16,7 @@ class Post extends Model
     const PUBLICADO=2;
 
     //relacion uno a muchos inversa
-    
+
     public function user(){
        return  $this->BelongsTo(User::class);
     }
@@ -24,7 +25,7 @@ class Post extends Model
     }
 
     //relacion muchos a muchos
-    
+
     public function tags(){
        return $this->BelongsToMany(Tag::class);
     }
@@ -32,6 +33,32 @@ class Post extends Model
     //Relacion uno a muchos polimorfica
     public function images(){
         return $this->morphMany(Image::class, 'imageable');
+    }
+
+
+    public function scopeIncluded(Builder $query)
+    {
+
+        if (empty($this->allowIncluded) || empty(request('included'))) { // validamos que la lista blanca y la variable included enviada a travez de HTTP no este en vacia.
+            return;
+        }
+
+
+        $relations = explode(',', request('included')); //['posts','relation2']//recuperamos el valor de la variable included y separa sus valores por una coma
+
+        // return $relations;
+
+        $allowIncluded = collect($this->allowIncluded); //colocamos en una colecion lo que tiene $allowIncluded en este caso = ['posts','posts.user']
+
+        foreach ($relations as $key => $relationship) { //recorremos el array de relaciones
+
+            if (!$allowIncluded->contains($relationship)) {
+                unset($relations[$key]);
+            }
+        }
+
+        $query->with($relations); //se ejecuta el query con lo que tiene $relations en ultimas es el valor en la url de included
+
     }
 
 
--- routes/api-v1.php
+++ routes/api-v1.php
@@ -5,6 +5,8 @@ use Illuminate\Routing\Route as RoutingRoute;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\PostController;
 
 
 //Route::post('register', [RegisterController::class,'store'])->name('api.v1.registrer');
@@ -14,6 +16,11 @@ use App\Http\Controllers\Api\CategoryController;
   Route::get('category/{category}', [CategoryController::class,'show'])->name('api.v1.categories.show');
   Route::put('categories/{category}', [CategoryController::class,'update'])->name('api.v1.categories.update');
   Route::delete('categories/{category}', [CategoryController::class,'destroy'])->name('api.v1.categories.delete');
+
+
+  Route::get('users', [UserController::class,'index'])->name('api.v1.posts.index');
+
+  Route::get('posts', [PostController::class,'index'])->name('api.v1.posts.index');
 
 
 //Route::apiResource('categories',CategoryController::class)->names('api.v1.categories');
